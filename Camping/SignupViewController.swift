bb6cfcad899bda477d90ff740d2caa125daeb701
okchuri <okchuri@gmail.com>
2015-08-01 02:17:19 +0900

    Create Sign Up View

---
+++ Camping/SignupViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignupViewController.swift
+//  Camping
+//
+//  Created by okchuri on 2015. 8. 1..
+//  Copyright (c) 2015년 okchuri. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    @IBAction func pressCancelButton(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
